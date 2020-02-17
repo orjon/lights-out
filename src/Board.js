240d3073ed8e54e6d2a0708ac44118114760ef38
orjon <github@orjon.com>
2020-02-17 00:00:51 +0000

    Initial commit. Random board generated and displayed

---
+++ src/Board.js
@@ -0,0 +1,126 @@
+import React, {Component} from "react";
+import Cell from "./Cell";
+import './Board.css';
+
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+class Board extends Component {
+  static defaultProps = {
+    nrows: 4,
+    ncols: 5,
+    chanceLightStartsOn: .20
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
+
+    // TODO: set initial state
+  }
+
+  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+
+  createBoard() {
+    let board = [];
+    for (let r=0; r < this.props.nrows; r++) {
+      let row = []
+      for (let c=0; c<this.props.ncols; c++){
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row)
+    }
+
+    console.log(board);  
+    // TODO: create array-of-arrays of true/false values
+
+    return board
+  }
+
+  /** handle changing a cell: update board & determine if winner */
+
+  flipCellsAround(coord) {
+    let {ncols, nrows} = this.props;
+    let board = this.state.board;
+    let [y, x] = coord.split("-").map(Number);
+
+
+    function flipCell(y, x) {
+      // if this coord is actually on board, flip it
+
+      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+        board[y][x] = !board[y][x];
+      }
+    }
+
+    // TODO: flip this cell and the cells around it
+
+    // win when every cell is turned off
+    // TODO: determine is the game has been won
+
+    // this.setState({board, hasWon}); 
+  }
+
+  generateBoard() {
+    return this.state.board.map(row => (
+      <tr>
+        {row.map(cell => (
+          <Cell isLit={cell} />
+        ))}
+      </tr>
+    ))
+  }
+
+
+  /** Render game board or winning message. */
+
+  render() {
+    this.createBoard()
+    return(
+      <table>
+        <tbody>
+          {this.generateBoard()}
+        </tbody>
+      </table>
+    );
+    // if the game is won, just show a winning msg & render nothing else
+
+    // TODO
+
+    // make table board
+
+    // TODO
+  }
+}
+
+
+export default Board;
+
+
